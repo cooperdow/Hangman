ceb4aa34a787792ec91c9e7f9ba0b1aabc7c0497
samiberry <samantha.berry@MU-C1-09.mullen.pvt.k12.co.us>
2015-10-20 13:21:14 -0600

    Made the css connected

--- Hangman.js
+++ Hangman.js
@@ -1,5 +1,3 @@
-
-
 var phrase = ["goosebumps", "synergy","jazz","Stay in tounch","on the dot","you don't have the guts","it's over my head","i could eat a horse", "read betweeen the lines", "my fingers are crossed","he is pulling my leg","speak of the devil","play it by ear","you can say that again","i am feeling under the weather","over my dead body","it's a small world after all","it slipped my mind","get a life","cat got your tounge","piece of cake","take a hike", "back to the grind","keep me in the loop", "schitzophrenia","better over than under"]
 function greet()
 {
