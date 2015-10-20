b836957aa69aa8aa387704dae0ad3e730bc78d11
cooperdow <cooperdow@users.noreply.github.com>
2015-10-20 13:44:16 -0600

    Moved Kate's JS to official JS

--- Hangman.js
+++ Hangman.js
@@ -1,17 +1,35 @@
 var phrase = ["goosebumps", "synergy","jazz","der"]
 function greet()
-{
-    $("#hello").text("Hi " + document.form.fullName.value + "! Ready to play Hangman?");
-}
-
+        {
+        $("#hello").text("Hi " + document.form.fullName.value + "! Ready to play Hangman?");
+        }
 function blankify()
 {
-    alert("l");
-    var phraseLetters = [];
+    var phraseLetters = new Array();
+    var phraseAsBlanks = new Array(); 
     var tempPhrase = "Hey guys";
-    for (var i = 0, i < tempPhrase.length, i++)
+    for (var i = 0; i < tempPhrase.length; i++)
         {
-            phraseLetters.push(tempPhrase.substring(i, i++);
+        phraseLetters.push(tempPhrase.substring(i, i+1));
         }
-    $("#test").text("hey");
+    for (var i = 0; i < tempPhrase.length; i++)
+        {
+        if(tempPhrase.substring(i, i+1) == " ")
+            {
+            phraseAsBlanks.push("/");
+            }
+        else
+            {
+            phraseAsBlanks.push("_ ");
+            }
+        }
+    $('#test').text(phraseLetters);
+    $('#test2').append(phraseAsBlanks);
+    for (var i = 0; i < phraseLetters.length; i++)
+        {
+        if(phraseLetters[i] === document.form.fullName.value)
+            {
+            $('#test').text(document.form.fullName.value);
+            }
+        }
 }
